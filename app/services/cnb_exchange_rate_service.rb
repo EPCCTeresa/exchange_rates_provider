deb3f91507d800c9941f1d91d8e9cecf8c93c2f1
Teresa Simancas Fernández <teresasimancas@MacBook-Air-de-Teresa.local>
2025-04-02 12:11:01 +0200

    [FT-3] Add cache to Cnb exchange rate request

--- app/services/cnb_exchange_rate_service.rb
+++ app/services/cnb_exchange_rate_service.rb
@@ -1,13 +1,18 @@
 class CnbExchangeRateServiceError < StandardError; end
 
 class CnbExchangeRateService
+  CACHE_KEY = "cnb_exchange_rates"
 
   def initialize(country = nil)
     @country = country
   end
 
   def call
-    exchange_rates = fetch_and_cache_rates
+    exchange_rates = Rails.cache.read(CACHE_KEY)
+
+    if exchange_rates.nil?
+      exchange_rates = fetch_and_cache_rates
+    end
 
     if @country
       if exchange_rates[@country]
@@ -26,6 +31,7 @@ class CnbExchangeRateService
     client = Clients::CnbExchangeRates.new
     exchange_rates = client.fetch_exchange_rates
 
+    Rails.cache.write(CACHE_KEY, exchange_rates, expires_in: 1.hour)
     exchange_rates
   end
 end
